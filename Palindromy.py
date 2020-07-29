58965082deb4eee3f786fa5dbc4088e8e8f2e425
Przemek Soból <przemyslaw.sobol@gmail.com>
2020-07-29 22:03:23 +0200

    Printed few words to test def

--- Palindromy.py
+++ Palindromy.py
@@ -1,4 +1,10 @@
 def if_palindrome(s):
     return s == s[::-1]
 
+print(if_palindrome("potop"))
+print(if_palindrome("kajak"))
+print(if_palindrome("tata"))
+print(if_palindrome("rawar"))
+print(if_palindrome("rabarbar"))
+
 
