92fa9c8888e91287e15de2eff16b0571e0900f1d
Przemek Soból <przemyslaw.sobol@gmail.com>
2020-08-04 21:48:24 +0200

    Add logging info in def calculate

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,4 +1,5 @@
 import logging
+logging.basicConfig(level=logging.DEBUG)
 
 dzialanie = int(input("Podaj działanie posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
 
@@ -6,16 +7,19 @@ def calculate():
     a = int(input("Podaj pierwszy składnik: "))
     b =  int(input("Podaj drugi składnik: "))
     if dzialanie == 1:
-        print(f"Dodaję {a} i {b}")
+        logging.info(f"Dodaję {a} i {b}")
         print(f"Wynik to {a + b}")
     elif dzialanie == 2:
-        print(f"Od {a} odejmuję {b} ")
+        logging.info(f"Od {a} odejmuję {b} ")
         print(f"Wynik to {a - b}")
     elif dzialanie == 3:
-        print(f"Mnożę {a} i {b}")
+        logging.info(f"Mnożę {a} i {b}")
         print(f"Wynik to {a * b}")
     elif dzialanie == 4:
-        print(f"Dzielę {a} przez {b}")
-        print(f"Wynik to {a / b}")
+        if b == 0:
+            print("Cholero nie dziel przez zero!")
+        else:
+            logging.info(f"Dzielę {a} przez {b}")
+            print(f"Wynik to {a / b}")
 
 calculate()
