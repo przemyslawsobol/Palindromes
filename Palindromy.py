ad7306aded467a938259e0585d4137cb757969ef
Przemek Soból <przemyslaw.sobol@gmail.com>
2020-08-13 20:24:56 +0200

    Updated def if_palindrome(s)

--- Palindromy.py
+++ Palindromy.py
@@ -1,5 +1,13 @@
 def if_palindrome(s):
-    return s == s[::-1]
+    i = 0
+    j = len(s) - 1
+
+    while i < j:
+        if s[i] != s[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
 
 slowo = input("Wprowadź słowo: ")
 print(slowo)
